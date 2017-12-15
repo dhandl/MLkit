8c9d88da94076c79b99fb4ef8279e61fc1054aa6
David Michael Handl <david.handl@cern.ch>
2017-12-15 16:25:18 +0100

    generalized setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,21 @@
-setupATLAS -q
-lsetup "root 6.10.04-x86_64-slc6-gcc62-opt"
+#!/bin/bash
+
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 
 export WorkDir=`pwd`
 export PYTHONPATH=$PYTHONPATH:$WorkDir/python
-export PATH="/home/d/David.Handl/miniconda2/bin:$PATH"
 export PATH=$PATH:$WorkDir/scripts
-source activate testenv
 
+if [[ "$HOSTNAME" == *"lxplus"* ]]; then
+  echo "Setting up environment on lxplus..."
+  export PATH=/afs/cern.ch/work/d/dhandl/public/miniconda3/bin:$PATH
+elif [[ "$HOSTNAME" == *"etp"* ]]; then
+  echo "Setting up environment on etp..."
+  export PATH=/project/etp5/dhandl/miniconda3/bin:$PATH
+else
+  echo "Setting up environment on c2pap..."
+ export PATH=/gpfs/scratch/pr62re/di36jop/workareas/miniconda3/bin:$PATH
+fi
+
+source activate testenv
