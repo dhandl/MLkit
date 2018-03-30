39aae14493e0abe3720cb2749cffea10d2168104
David Michael Handl <david.handl@cern.ch>
2018-03-30 15:08:35 +0200

    minor changes

--- config/algorithm.py
+++ config/algorithm.py
@@ -5,13 +5,13 @@ Algorithm = namedtuple("Algorithm", "name modelname options")
 # define your ML algorithm here
 analysis = [
 Algorithm('NN',
-          'DNN_rmsprop_layer128_batch512_GlorotNormalInitializer',
+          'DNN_ADAM_layer5x64_batch16_GlorotNormalInitializer_dropout0p2',
           {
-          'layers':[128],
+          'layers':[64,64,64,64,64],
           'ncycles':100,
-          'batchSize':512,
-          'dropout':0.3,
-          'optimizer':'rmsprop',
+          'batchSize':16,
+          'dropout':0.2,
+          'optimizer':'ADAM',
           'activation':'relu',
           'initializer':'glorot_normal',
           'learningRate':0.01,
@@ -30,5 +30,26 @@ Algorithm('BDT',
            'n_estimators':850,
            'learning_rate':0.5
           }
-          ),
+),
+Algorithm('RNN',
+          'RNN_jetOnly_ADAM_LSTM25_1layer_batch512_GlorotNormalInitializer_dropout0p3',
+          {
+          'collection':['jet'],
+          'unit_type':'LSTM',
+          'n_units':25,
+          'combinedDim':[256,256,256,256,256],
+          'epochs':100,
+          'batchSize':512,
+          'dropout':0.3, 
+          'optimizer':'ADAM',
+          'activation':'relu',
+          'initializer':'glorot_normal', 
+          'learningRate':0.01,
+          'decay':0.0, 
+          'momentum':0.0,
+          'nesterov':False,
+          'mergeModels':False, 
+          'multiclassification':False
+          }
+),
 ]
