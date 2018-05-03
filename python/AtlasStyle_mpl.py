5cf383f40e806d800a5d7821dbc54ce5cbbceaf8
Alessandro.Schwemmer <alessandro.schwemmer@physik.uni-muenchen.de>
2018-05-03 11:13:33 +0200

    improved plotstyle

--- python/AtlasStyle_mpl.py
+++ python/AtlasStyle_mpl.py
@@ -42,7 +42,7 @@ def ATLASLabel(axes, x, y, text='Work in progress', color='black'):
   #bb = dict(boxstyle='square', fc='w', lw=0)
   atlas = axes.text(x, y, 'ATLAS', style='italic', weight='bold', size='x-large', color=color, transform=axes.transAxes)
   p = atlas.get_position()
-  add = axes.text(x+0.12, y, text, size='x-large', color=color, transform=axes.transAxes)
+  add = axes.text(x+0.17, y, text, size='x-large', color=color, transform=axes.transAxes)
 
 def LumiLabel(axes, x, y, lumi=100, color='black'):
   axes.text(x, y, '$\sqrt{\mathrm{s}}=$13 TeV, L$=$'+str(lumi)+'fb$^{-1}$', size='x-large', transform=axes.transAxes)
