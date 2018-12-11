39380e1268dfd3952434387a014c6902e773f7e8
David Michael Handl <david.handl@cern.ch>
2018-12-11 10:30:16 +0100

    minor

--- config/samples.py
+++ config/samples.py
@@ -2,62 +2,89 @@ from collections import namedtuple
 
 #input = '/gpfs/scratch/pr62re/di36jop/samples/ttbar_rew/TRUTH3/hdf5/'
 #input = '/project/etp5/dhandl/samples/ttbar_rew/TRUTH3/hdf5/'
-input = '/project/etp5/dhandl/samples/SUSY/Stop1L/hdf5/cut_mt30_met60_preselection/'
+inputMC15 = '/project/etp5/dhandl/samples/SUSY/Stop1L/EarlyRun2/hdf5/cut_mt30_met60_preselection/'
+inputMC16 = '/project/etp5/dhandl/samples/SUSY/Stop1L/FullRun2/hdf5/cut_mt30_met60_preselection_new/'
+
+testinput = '/project/etp5/dhandl/samples/SUSY/Stop1L/FullRun2/hdf5/test/'
 
 # define your samples here
+SignalMC16 = [
+  {'name':'stop_bWN_450_300_truth', 'path':inputMC16+'stop_bWN_450_300_truth/'}
+  #{'name':'stop_bWN_450_300_mc16a', 'path':inputMC16+'stop_bWN_450_300_mc16a/'},
+  #{'name':'stop_bWN_450_300_mc16d', 'path':inputMC16+'stop_bWN_450_300_mc16d/'}
+  #{'name':'stop_bWN_450_300', 'path':inputMC16+'stop_bWN_450_300_TruthSmeared/'}
+  #{'name':'stop_bWN_450_300_truth', 'path':testinput+'stop_bWN_450_300_truth/'}
+]
+
 Signal1 = [
   #{'name':'powheg_ttbar_TRUTH3', 'path':input+'powheg_ttbar_TRUTH3/'}
-  {'name':'stop_bWN_250_100', 'path':input+'stop_bWN_250_100/'},
-  {'name':'stop_bWN_300_150', 'path':input+'stop_bWN_300_150/'},
-  {'name':'stop_bWN_350_200', 'path':input+'stop_bWN_350_200/'},
-  {'name':'stop_bWN_400_250', 'path':input+'stop_bWN_400_250/'},
-  {'name':'stop_bWN_450_300', 'path':input+'stop_bWN_450_300/'},
-  {'name':'stop_bWN_500_350', 'path':input+'stop_bWN_500_350/'},
-  {'name':'stop_bWN_550_400', 'path':input+'stop_bWN_550_400/'},
-  {'name':'stop_bWN_600_450', 'path':input+'stop_bWN_600_450/'},
-  {'name':'stop_bWN_650_500', 'path':input+'stop_bWN_650_500/'}
+  {'name':'stop_bWN_250_100', 'path':inputMC15+'stop_bWN_250_100/'},
+  {'name':'stop_bWN_300_150', 'path':inputMC15+'stop_bWN_300_150/'},
+  {'name':'stop_bWN_350_200', 'path':inputMC15+'stop_bWN_350_200/'},
+  {'name':'stop_bWN_400_250', 'path':inputMC15+'stop_bWN_400_250/'},
+  {'name':'stop_bWN_450_300', 'path':inputMC15+'stop_bWN_450_300/'},
+  {'name':'stop_bWN_500_350', 'path':inputMC15+'stop_bWN_500_350/'},
+  {'name':'stop_bWN_550_400', 'path':inputMC15+'stop_bWN_550_400/'},
+  {'name':'stop_bWN_600_450', 'path':inputMC15+'stop_bWN_600_450/'},
+  {'name':'stop_bWN_650_500', 'path':inputMC15+'stop_bWN_650_500/'}
 ]
 
 
 Signal2 = [
   #{'name':'powheg_ttbar_TRUTH3', 'path':input+'powheg_ttbar_TRUTH3/'}
-  {'name':'stop_bWN_250_130', 'path':input+'stop_bWN_250_130/'},
-  {'name':'stop_bWN_300_180', 'path':input+'stop_bWN_300_180/'},
-  {'name':'stop_bWN_350_230', 'path':input+'stop_bWN_350_230/'},
-  {'name':'stop_bWN_400_280', 'path':input+'stop_bWN_400_280/'},
-  {'name':'stop_bWN_450_330', 'path':input+'stop_bWN_450_330/'},
-  {'name':'stop_bWN_500_380', 'path':input+'stop_bWN_500_380/'},
-  {'name':'stop_bWN_550_430', 'path':input+'stop_bWN_550_430/'},
-  {'name':'stop_bWN_600_480', 'path':input+'stop_bWN_600_480/'},
-  {'name':'stop_bWN_650_530', 'path':input+'stop_bWN_650_530/'}
+  {'name':'stop_bWN_250_130', 'path':inputMC15+'stop_bWN_250_130/'},
+  {'name':'stop_bWN_300_180', 'path':inputMC15+'stop_bWN_300_180/'},
+  {'name':'stop_bWN_350_230', 'path':inputMC15+'stop_bWN_350_230/'},
+  {'name':'stop_bWN_400_280', 'path':inputMC15+'stop_bWN_400_280/'},
+  {'name':'stop_bWN_450_330', 'path':inputMC15+'stop_bWN_450_330/'},
+  {'name':'stop_bWN_500_380', 'path':inputMC15+'stop_bWN_500_380/'},
+  {'name':'stop_bWN_550_430', 'path':inputMC15+'stop_bWN_550_430/'},
+  {'name':'stop_bWN_600_480', 'path':inputMC15+'stop_bWN_600_480/'},
+  {'name':'stop_bWN_650_530', 'path':inputMC15+'stop_bWN_650_530/'}
 ]
 
 Signal3 = [
   #{'name':'powheg_ttbar_TRUTH3', 'path':input+'powheg_ttbar_TRUTH3/'}
-  {'name':'stop_bWN_250_160', 'path':input+'stop_bWN_250_160/'},
-  {'name':'stop_bWN_300_210', 'path':input+'stop_bWN_300_210/'},
-  {'name':'stop_bWN_350_260', 'path':input+'stop_bWN_350_260/'},
-  {'name':'stop_bWN_400_310', 'path':input+'stop_bWN_400_310/'},
-  {'name':'stop_bWN_450_360', 'path':input+'stop_bWN_450_360/'},
-  {'name':'stop_bWN_550_460', 'path':input+'stop_bWN_550_460/'},
-  {'name':'stop_bWN_600_510', 'path':input+'stop_bWN_600_510/'},
-  {'name':'stop_bWN_650_560', 'path':input+'stop_bWN_650_560/'}
+  {'name':'stop_bWN_250_160', 'path':inputMC15+'stop_bWN_250_160/'},
+  {'name':'stop_bWN_300_210', 'path':inputMC15+'stop_bWN_300_210/'},
+  {'name':'stop_bWN_350_260', 'path':inputMC15+'stop_bWN_350_260/'},
+  {'name':'stop_bWN_400_310', 'path':inputMC15+'stop_bWN_400_310/'},
+  {'name':'stop_bWN_450_360', 'path':inputMC15+'stop_bWN_450_360/'},
+  {'name':'stop_bWN_550_460', 'path':inputMC15+'stop_bWN_550_460/'},
+  {'name':'stop_bWN_600_510', 'path':inputMC15+'stop_bWN_600_510/'},
+  {'name':'stop_bWN_650_560', 'path':inputMC15+'stop_bWN_650_560/'}
+]
+
+Signal4 = [
+  #{'name':'powheg_ttbar_TRUTH3', 'path':input+'powheg_ttbar_TRUTH3/'}
+  {'name':'stop_bWN_350_185', 'path':inputMC15+'stop_bWN_350_185/'},
+  {'name':'stop_bWN_400_235', 'path':inputMC15+'stop_bWN_400_235/'},
+  {'name':'stop_bWN_450_285', 'path':inputMC15+'stop_bWN_450_285/'},
+  {'name':'stop_bWN_500_335', 'path':inputMC15+'stop_bWN_500_335/'},
+  {'name':'stop_bWN_550_385', 'path':inputMC15+'stop_bWN_550_385/'},
+  {'name':'stop_bWN_600_435', 'path':inputMC15+'stop_bWN_600_435/'},
+  {'name':'stop_bWN_650_485', 'path':inputMC15+'stop_bWN_650_485/'}
 ]
 
 
 #Signal1.extend(Signal2)
 #Signal1.extend(Signal3)
 
-Signal = Signal1
+Signal = SignalMC16
 
 Signal_Evaluate = Signal1
 Signal_Evaluate.extend(Signal2)
 Signal_Evaluate.extend(Signal3)
+Signal_Evaluate.extend(Signal4)
 
 Background = [
   #{'name':'ttbar_radHi_TRUTH3',  'path':input+'TestBkg/'}
   #{'name':'ttbar_radLo_TRUTH3', 'path':input+'ttbar_radLo_TRUTH3/'}
-  {'name':'powheg_ttbar', 'path':input+'powheg_ttbar/'},
-  {'name':'powheg_singletop', 'path':input+'powheg_singletop/'},
-  {'name':'sherpa22_Wjets', 'path':input+'sherpa22_Wjets/'}
+  {'name':'mc16a_ttbar', 'path':inputMC16+'mc16a_ttbar/'},
+  {'name':'mc16d_ttbar', 'path':inputMC16+'mc16d_ttbar/'},
+  {'name':'mc16a_singletop', 'path':inputMC16+'mc16a_singletop/'},
+  {'name':'mc16d_singletop', 'path':inputMC16+'mc16d_singletop/'},
+  {'name':'mc16a_wjets', 'path':inputMC16+'mc16a_wjets/'},
+  {'name':'mc16d_wjets', 'path':inputMC16+'mc16d_wjets/'}
+  #{'name':'mc16a_ttbar', 'path':testinput+'mc16a_ttbar/'},
 ]
--- config/variables.py
+++ config/variables.py
@@ -2,118 +2,124 @@
 preselection = [ 
                 {'name':'n_jet',  'threshold':4,      'type':'geq'},
                 {'name':'n_bjet',  'threshold':1,      'type':'geq'},
-                {'name':'met',    'threshold':100e3,  'type':'geq'},
-                {'name':'mt',    'threshold':90e3,  'type':'geq'},
-                {'name':'n_lep',  'threshold':1,      'type':'exact'}
-                #{'name':'dphi_jet0_ptmiss', 'threshold':1.8,    'type':'geq'},
-                #{'name':'dphi_jet1_ptmiss', 'threshold':0.4,    'type':'greater'}
-                #{'name':'ht',   'threshold':200e3,  'type':'geq'},
-                #{'name':'dphi_b_lep_max',   'threshold':2.8,  'type':'leq'}
-                #{'name':'dphi_b_lep_max',   'threshold':2.6,  'type':'condition',   'variable':'ht',   'lessthan':225e3,   'morethan': 275e3}
-                #{'name':'dr_bjet_lep',    'threshold':2.5,  'type':'leq'}
-                #{'name':'dphi_met_lep',    'threshold':1.8,  'type':'geq'}
-                #{'name':'tt_cat_TRUTH3',  'threshold':0,      'type':'geq'},
-                #{'name':'tt_cat_TRUTH3',  'threshold':6,      'type':'leq'},
-                #{'name':'tt_cat_TRUTH3',  'threshold':1,      'type':'not'},
-                #{'name':'tt_cat_TRUTH3',  'threshold':4,      'type':'not'}
+                {'name':'met',    'threshold':230e3,  'type':'geq'},
+                {'name':'mt',    'threshold':110e3,  'type':'geq'},
+                {'name':'n_lep',  'threshold':1,      'type':'exact'},
+                {'name':'lep_pt',  'threshold':25e3,      'type':'geq'}
 ]
 
 preselection_evaluate = [ 
                 {'name':'n_jet',  'threshold':4,      'type':'geq'},
                 {'name':'n_bjet',  'threshold':1,      'type':'geq'},
-                {'name':'met',    'threshold':250e3,  'type':'geq'},
-                {'name':'mt',    'threshold':90e3,  'type':'geq'},
-                {'name':'n_lep',  'threshold':1,      'type':'exact'}
-                #{'name':'dphi_jet0_ptmiss', 'threshold':0.4,    'type':'greater'},
-                #{'name':'dphi_jet1_ptmiss', 'threshold':0.4,    'type':'greater'}
-                #{'name':'ht',   'threshold':200e3,  'type':'geq'},
-                #{'name':'dphi_b_lep_max',   'threshold':2.8,  'type':'leq'}
-                #{'name':'dphi_b_lep_max',   'threshold':2.6,  'type':'condition',   'variable':'ht',   'lessthan':225e3,   'morethan': 275e3}
-                #{'name':'dr_bjet_lep',    'threshold':2.5,  'type':'leq'}
-                #{'name':'dphi_met_lep',    'threshold':2,  'type':'leq'}
-                #{'name':'tt_cat_TRUTH3',  'threshold':0,      'type':'geq'},
-                #{'name':'tt_cat_TRUTH3',  'threshold':6,      'type':'leq'},
-                #{'name':'tt_cat_TRUTH3',  'threshold':1,      'type':'not'},
-                #{'name':'tt_cat_TRUTH3',  'threshold':4,      'type':'not'}
+                {'name':'met',    'threshold':230e3,  'type':'geq'},
+                {'name':'mt',    'threshold':110e3,  'type':'geq'},
+                {'name':'n_lep',  'threshold':1,      'type':'exact'},
+                {'name':'lep_pt',  'threshold':25e3,      'type':'geq'}
 ]
 
 lumi = 140e3
 
 #High level variables
 
-nvar = [
-        'ht',
-        #'jet_pt[0]',
-        'bjet_pt[0]',
-        'amt2',
-        'mt',
-        'met',
-        'dphi_met_lep',
-        'dphi_b_lep_max',
-        'dphi_jet0_ptmiss',
-        #'met_sig',
-        #'met_proj_lep',
-        #'ht_sig',
-        'm_bl',
-        'dr_bjet_lep'
-        #'mT_blMET' #15vars
-        #'n_jet',
-        #'n_bjet'
-        #'jet_pt[1]',
-        #'jet_pt[2]',
-        #'jet_pt[3]',
-        #'lep_pt[0]',
-        #'dr_jet_jet_max',
-        #'ttbar_pt',
-        #'ttbar_dphi',
-        #'m_jet1_jet2',
-        #'m_jet_jet_min',
-        #'m_jet_jet_max'
-        #'tt_cat_TRUTH3'
-        #'n_lep',
-        #'lep_eta[0]',
-        #'lep_phi[0]',
-        #'jet_eta[0]',
-        #'jet_phi[0]',
-        #'dphi_jet1_ptmiss',
-        #'dphi_jet2_ptmiss',
-        #'dphi_jet3_ptmiss',
-        #'dphi_min_ptmiss',
-        #'dphi_b_ptmiss_max'
-        ]
+#nvar = [
+##        #'ht',
+#        'jet_pt[0]',
+#        'bjet_pt[0]',
+#        'amt2',
+#        'mt',
+#        'met',
+##        #'met_phi',
+#        'dphi_met_lep',
+##        #'dphi_b_lep_max',
+##        #'dphi_jet0_ptmiss',
+##        #'met_sig',
+#        'met_proj_lep',
+#        'ht_sig',
+#        'm_bl',
+#        'lepPt_over_met'
+##        #'dr_bjet_lep',
+##        #'mT_blMET', #15vars
+##        'n_jet'
+##        #'n_bjet'
+##        #'jet_pt[1]',
+##        #'jet_pt[2]',
+##        #'jet_pt[3]',
+##        #'lep_pt[0]',
+##        #'dr_jet_jet_max',
+##        #'ttbar_pt',
+##        #'ttbar_dphi',
+##        #'m_jet1_jet2',
+##        #'m_jet_jet_min',
+##        #'m_jet_jet_max'
+##        #'tt_cat_TRUTH3'
+##        #'n_lep',
+##        #'lep_pt',
+##        #'lep_eta',
+##        #'lep_phi',
+##        #'lep_e'
+##        #'jet_eta[0]',
+##        #'jet_phi[0]',
+##        #'dphi_jet1_ptmiss',
+##        #'dphi_jet2_ptmiss',
+##        #'dphi_jet3_ptmiss',
+##        #'dphi_min_ptmiss',
+##        #'dphi_b_ptmiss_max'
+#        ]
 
 #Low level variables
 
-#nvar = [
-        #'met',
-        #'met_phi',
-        #'n_jet',
-        #'n_bjet',
+nvar = [
+        'met',
+        'met_phi',
+        'dphi_met_lep',
+        'mt',
+        'n_jet',
+        'n_bjet',
         #'jet_pt[0]',
         #'jet_eta[0]',
         #'jet_phi[0]',
         #'jet_e[0]',
-        #'jet_mv2c10[0]',
         #'jet_pt[1]',
         #'jet_eta[1]',
         #'jet_phi[1]',
         #'jet_e[1]',
-        #'jet_mv2c10[1]',
         #'jet_pt[2]',
         #'jet_eta[2]',
         #'jet_phi[2]',
         #'jet_e[2]',
-        #'jet_mv2c10[2]',
         #'jet_pt[3]',
         #'jet_eta[3]',
         #'jet_phi[3]',
         #'jet_e[3]',
-        #'jet_mv2c10[3]',
-        #'lep_pt[0]',
-        #'lep_eta[0]',
-        #'lep_phi[0]',
-        #'lep_e[0]'
-#]
+        #'jet_pt[4]',
+        #'jet_eta[4]',
+        #'jet_phi[4]',
+        #'jet_e[4]',
+        #'jet_pt[5]',
+        #'jet_eta[5]',
+        #'jet_phi[5]',
+        #'jet_e[5]',
+        #'jet_pt[6]',
+        #'jet_eta[6]',
+        #'jet_phi[6]',
+        #'jet_e[6]',
+        #'jet_pt[7]',
+        #'jet_eta[7]',
+        #'jet_phi[7]',
+        #'jet_e[7]',
+        #'jet_pt[8]',
+        #'jet_eta[8]',
+        #'jet_phi[8]',
+        #'jet_e[8]',
+        #'jet_pt[9]',
+        #'jet_eta[9]',
+        #'jet_phi[9]',
+        #'jet_e[9]',
+        'lep_pt[0]',
+        'lep_eta[0]',
+        'lep_phi[0]',
+        'lep_e[0]'
+]
 
 weight = [
           'weight',
