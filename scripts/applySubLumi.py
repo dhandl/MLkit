6a2c5efde1db0b06dc5604f41d9c8308e35c8768
David Michael Handl <david.handl@cern.ch>
2019-01-23 10:10:15 +0100

    apply a friend tree for the sub-campaign lumi

---
+++ scripts/applySubLumi.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+import ROOT
+import os
+import sys
+from array import array
+
+TOTAL_LUMI = (3219.56 + 32988.1 + 44307.4 + 59937.2)
+LUMI_FRACTION = [(3219.56 + 32988.1)/TOTAL_LUMI, (44307.4)/TOTAL_LUMI, (59937.2)/TOTAL_LUMI]
+
+LUMI_ARRAY = array("f", [0])
+
+def filesize(num):
+  for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+    if abs(num) < 1024.0:
+      return "%3.1f%sB" % (num, unit)
+    num /= 1024.0
+  return "%.1fYiB" % num
+
+def main():
+  global TOTAL_LUMI
+  global LUMI_ARRAY
+
+  if len(sys.argv)>2:
+    print "Usage: applySubLumi.py <source directory>"
+    return
+
+  src = sys.argv[1]
+
+  if not os.path.isdir(src):
+    print "No such folder '{}'".format(src)
+    return
+
+  # get all .root files in all subdirectories of <src>
+  inFiles = [os.path.relpath(os.path.join(d, f), src) for (d, _, files) in os.walk(src) for f in files if f.endswith("root")] 
+
+  for f in inFiles:
+    print "\nApplying lumi weight on file: {}".format(os.path.join(src,f))
+
+    fSrc= os.path.join(src, f)
+
+    f = ROOT.TFile(fSrc, "UPDATE")
+
+    # Get all trees in this file
+    for name in set([k.GetName() for k in f.GetListOfKeys() if k.GetClassName() == "TTree"]):
+      if ("_ML" in name) or ("_lumi" in name):
+        print "Skipping {} ...".format(name)
+        continue
+
+      t = f.Get(name)
+      original_tree = name
+      friendName = t.GetName()+"_lumi"
+
+      sub_campaign = original_tree[original_tree.find("mc16") + 4]
+      if sub_campaign == "a":
+        lumi_weight = LUMI_FRACTION[0]
+      elif sub_campaign == "d":
+        lumi_weight = LUMI_FRACTION[1]
+      elif sub_campaign == "e":
+        lumi_weight = LUMI_FRACTION[2]
+      
+      print "\tFound sub-campaign: {}\n\tWill apply lumi weight: {}".format("mc16"+sub_campaign, lumi_weight)
+    
+      friendTree = ROOT.TTree(friendName, friendName)
+
+      lumi_branch = friendTree.Branch("weight_lumi", LUMI_ARRAY, "weight_lumi/F")
+
+      n = t.GetEntries()
+      for i in xrange(n):
+        if (i%100000 == 0):
+          print "At %i-th event of %i total events in sample %s!"%(i,n,name)
+        LUMI_ARRAY[0] = lumi_weight
+        friendTree.Fill()
+
+      friendTree.Write("", ROOT.TObject.kOverwrite)
+      t.AddFriend(friendName)
+      t.Write(t.GetName(), ROOT.TObject.kOverwrite)
+      f.Close()
+
+
+if __name__ == "__main__":
+  main()
