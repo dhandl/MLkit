9c7b4a67d32ee5d3a776f2c6d422c211bb317869
David Michael Handl <david.handl@cern.ch>
2019-01-30 09:16:30 +0100

    add lumi branch to tuples

--- scripts/applySubLumi.py
+++ scripts/applySubLumi.py
@@ -7,7 +7,7 @@ from array import array
 
 TOTAL_LUMI = (3219.56 + 32988.1 + 44307.4 + 59937.2)
 LUMI_FRACTION = [(3219.56 + 32988.1)/TOTAL_LUMI, (44307.4)/TOTAL_LUMI, (59937.2)/TOTAL_LUMI]
-
+LUMI_BRANCH_NAME = "lumi_weight"
 LUMI_ARRAY = array("f", [0])
 
 def filesize(num):
@@ -20,6 +20,7 @@ def filesize(num):
 def main():
   global TOTAL_LUMI
   global LUMI_ARRAY
+  global LUMI_BRANCH_NAME
 
   if len(sys.argv)>2:
     print "Usage: applySubLumi.py <source directory>"
@@ -43,15 +44,21 @@ def main():
 
     # Get all trees in this file
     for name in set([k.GetName() for k in f.GetListOfKeys() if k.GetClassName() == "TTree"]):
-      if ("_ML" in name) or ("_lumi" in name):
+      if ("data" in name):
         print "Skipping {} ...".format(name)
         continue
 
       t = f.Get(name)
       original_tree = name
-      friendName = t.GetName()+"_lumi"
+      #friendName = t.GetName()+"_lumi"
 
-      sub_campaign = original_tree[original_tree.find("mc16") + 4]
+      branches = [b.GetName() for b in t.GetListOfBranches()]
+      inList = filter(lambda x: x == LUMI_BRANCH_NAME, branches)
+      if len(inList) > 0:
+        print "Skipping {} ...".format(name)
+        continue
+      
+      sub_campaign = fSrc[fSrc.find("mc16") + 4]
       if sub_campaign == "a":
         lumi_weight = LUMI_FRACTION[0]
       elif sub_campaign == "d":
@@ -61,22 +68,20 @@ def main():
       
       print "\tFound sub-campaign: {}\n\tWill apply lumi weight: {}".format("mc16"+sub_campaign, lumi_weight)
     
-      friendTree = ROOT.TTree(friendName, friendName)
-
-      lumi_branch = friendTree.Branch("weight_lumi", LUMI_ARRAY, "weight_lumi/F")
+      lumi_branch = t.Branch(LUMI_BRANCH_NAME, LUMI_ARRAY, LUMI_BRANCH_NAME+"/F")
 
       n = t.GetEntries()
       for i in xrange(n):
         if (i%100000 == 0):
           print "At %i-th event of %i total events in sample %s!"%(i,n,name)
         LUMI_ARRAY[0] = lumi_weight
-        friendTree.Fill()
+        lumi_branch.Fill()
 
-      friendTree.Write("", ROOT.TObject.kOverwrite)
-      t.AddFriend(friendName)
-      t.Write(t.GetName(), ROOT.TObject.kOverwrite)
-      f.Close()
-
+      #friendTree.Write("", ROOT.TObject.kOverwrite)
+      #t.AddFriend(friendName)
+      t.Write(name, ROOT.TObject.kOverwrite)
+    #f.Write()
+    f.Close()
 
 if __name__ == "__main__":
   main()
