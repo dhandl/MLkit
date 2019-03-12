546ef3c07166aca03c22d78c4bd69096bc19e1c5
David Michael Handl <david.handl@cern.ch>
2019-03-12 13:35:04 +0100

    minor

--- python/startPlot.py
+++ python/startPlot.py
@@ -1,4 +1,4 @@
-import sys
+import os,sys
 from sklearn.preprocessing import StandardScaler
 import h5py
 from keras.models import load_model
@@ -58,21 +58,6 @@ def startPlot(modelDir, binning=[50,0,1.], save=False, multiclass=True):
             xdict['morethan'] = float(x.split()[10])
         preselection.append(xdict)
         
-    preselection_no_met = [ 
-                {'name':'n_jet',  'threshold':4,      'type':'geq'},
-                {'name':'n_bjet',  'threshold':1,      'type':'geq'},
-                {'name':'mt',    'threshold':90e3,  'type':'geq'},
-                {'name':'n_lep',  'threshold':1,      'type':'exact'}
-                ]
-    
-    preselection_special = [ 
-                {'name':'n_jet',  'threshold':4,      'type':'geq'},
-                {'name':'n_bjet',  'threshold':1,      'type':'geq'},
-                {'name':'met',    'threshold':250e3,  'type':'geq'},
-                {'name':'mt',    'threshold':90e3,  'type':'geq'},
-                {'name':'n_lep',  'threshold':1,      'type':'exact'}
-                ]
-        
     print 'Loading dataset...'
     datasetDir = 'TrainedModels/datasets/' + infos[3].replace('Used dataset: ', '').replace('\n','') + '.h5'
     
@@ -82,58 +67,22 @@ def startPlot(modelDir, binning=[50,0,1.], save=False, multiclass=True):
     
     print 'Using dataset from:', datasetDir
     
-    #print 'Defining files for specific confusion matrices...'
-    
-    #Signal = []
-    #input = '/project/etp5/dhandl/samples/SUSY/Stop1L/EarlyRun2/hdf5/cut_mt30_met60_preselection/'
-    
-    #point1 = [{'name':'stop_bWN_250_100', 'path':input+'stop_bWN_250_100/'},{'name':'stop_bWN_250_130', 'path':input+'stop_bWN_250_130/'},{'name':'stop_bWN_250_160', 'path':input+'stop_bWN_250_160/'}]
-    #point2 = [{'name':'stop_bWN_300_150', 'path':input+'stop_bWN_300_150/'},{'name':'stop_bWN_300_180', 'path':input+'stop_bWN_300_180/'},{'name':'stop_bWN_300_210', 'path':input+'stop_bWN_300_210/'}]
-    #point3 = [{'name':'stop_bWN_350_200', 'path':input+'stop_bWN_350_200/'},{'name':'stop_bWN_350_230', 'path':input+'stop_bWN_350_230/'},{'name':'stop_bWN_350_260', 'path':input+'stop_bWN_350_260/'}]
-    #point4 = [{'name':'stop_bWN_400_250', 'path':input+'stop_bWN_400_250/'},{'name':'stop_bWN_400_280', 'path':input+'stop_bWN_400_280/'},{'name':'stop_bWN_400_310', 'path':input+'stop_bWN_400_310/'}]
-    #point5 = [{'name':'stop_bWN_450_300', 'path':input+'stop_bWN_450_300/'},{'name':'stop_bWN_450_330', 'path':input+'stop_bWN_450_330/'},{'name':'stop_bWN_450_360', 'path':input+'stop_bWN_450_360/'}]
-    #point6 = [{'name':'stop_bWN_500_350', 'path':input+'stop_bWN_500_350/'},{'name':'stop_bWN_500_380', 'path':input+'stop_bWN_500_380/'}]
-    #point7 = [{'name':'stop_bWN_550_400', 'path':input+'stop_bWN_550_400/'},{'name':'stop_bWN_550_430', 'path':input+'stop_bWN_550_430/'},{'name':'stop_bWN_550_460', 'path':input+'stop_bWN_550_460/'}]
-    #point8 = [{'name':'stop_bWN_600_450', 'path':input+'stop_bWN_600_450/'},{'name':'stop_bWN_600_480', 'path':input+'stop_bWN_600_480/'},{'name':'stop_bWN_600_510', 'path':input+'stop_bWN_600_510/'}]
-    #point9 = [{'name':'stop_bWN_650_500', 'path':input+'stop_bWN_650_500/'},{'name':'stop_bWN_650_530', 'path':input+'stop_bWN_650_530/'},{'name':'stop_bWN_650_560', 'path':input+'stop_bWN_650_560/'}]
-    
-    #Signal1 = [
-    #[{'name':'stop_bWN_250_100', 'path':input+'stop_bWN_250_100/'}],
-    #[{'name':'stop_bWN_300_150', 'path':input+'stop_bWN_300_150/'}],
-    #[{'name':'stop_bWN_350_200', 'path':input+'stop_bWN_350_200/'}],
-    #[{'name':'stop_bWN_400_250', 'path':input+'stop_bWN_400_250/'}],
-    #[{'name':'stop_bWN_450_300', 'path':input+'stop_bWN_450_300/'}],
-    #[{'name':'stop_bWN_500_350', 'path':input+'stop_bWN_500_350/'}],
-    #[{'name':'stop_bWN_550_400', 'path':input+'stop_bWN_550_400/'}],
-    #[{'name':'stop_bWN_600_450', 'path':input+'stop_bWN_600_450/'}],
-    #[{'name':'stop_bWN_650_500', 'path':input+'stop_bWN_650_500/'}]
-    #]
-    
-    #Signal.append(point1)
-    #Signal.append(point2)
-    #Signal.append(point3)
-    #Signal.append(point4)
-    #Signal.append(point5)
-    #Signal.append(point6)
-    #Signal.append(point7)
-    #Signal.append(point8)
-    #Signal.append(point9)
-    
     print 'Loading model...'
     
-    try:
-        pickleDir = modelDir.replace('.h5', '_history.pkl')
-        model = load_model(modelDir)
-        model.load_weights(modelDir.replace('.h5' , '_weights.h5').replace('models' , 'weights'))
-        if analysis.lower() == 'rnn':
-          print('Recurrent Neural Network detected!')
-        else:
-          print('Neural Network detected!')
-          print('Scaling and reading values...')
-    except IOError:
-        model = joblib.load(modelDir)
-        print('Boosted Decision Tree detected!')
-        return 0
+    if analysis.lower() == 'rnn':
+      print('Recurrent Neural Network detected!')
+      pickleDir = modelDir.replace('.h5', '_history.pkl')
+      model = load_model(modelDir)
+      model.load_weights(modelDir.replace('.h5' , '_weights.h5').replace('models' , 'weights'))
+    elif analysis.lower() == 'nn':
+      print('Neural Network detected!')
+      pickleDir = modelDir.replace('.h5', '_history.pkl')
+      model = load_model(modelDir)
+      model.load_weights(modelDir.replace('.h5' , '_weights.h5').replace('models' , 'weights'))
+
+    elif analysis.lower() == 'bdt':
+      print('Boosted Decision Tree detected!')
+      model = joblib.load(modelDir)
     
     #Get the data and scale it, if necessary
     print 'Loading data from dataset...'
@@ -151,22 +100,30 @@ def startPlot(modelDir, binning=[50,0,1.], save=False, multiclass=True):
       seq = getSequence(parameters)
       for idx, c in enumerate(seq):
         sequence.append({'name':c, 'X_train':dataset['X_train_'+c][:], 'X_test':dataset['X_test_'+c][:]})
+
+    if os.path.exists(modelDir.replace('.h5' , '_scaler.pkl')):
+      scaler = joblib.load(modelDir.replace('.h5' , '_scaler.pkl')) # RNN sequences already scaled!
+      X_train_scaled = scaler.transform(X_train)
+      X_test_scaled = scaler.transform(X_test)
+      X_scaled = scaler.transform(X)
+    else:
+      X_train_scaled = X_train
+      X_test_scaled = X_test
+      X_scaled = X
     
-    scaler = joblib.load(modelDir.replace('.h5' , '_scaler.pkl')) # RNN sequences already scaled!
-
-    X_train_scaled = scaler.transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
-    X_scaled = scaler.transform(X)
-
     if analysis == 'rnn':
       y_predict_train = model.predict([s['X_train'] for s in sequence] + [X_train_scaled])
       y_predict_test = model.predict([s['X_test'] for s in sequence] + [X_test_scaled])
       y_predict = np.concatenate((y_predict_train, y_predict_test))
       y = np.concatenate((y_train, y_test))
-    else:
+    elif analysis == 'nn':
       y_predict_train = model.predict(X_train_scaled)
       y_predict_test = model.predict(X_test_scaled)
       y_predict = model.predict(X_scaled)
+    elif analysis == 'bdt':
+      y_predict_train = model.predict_proba(X_train_scaled)
+      y_predict_test = model.predict_proba(X_test_scaled)
+      y_predict = model.predict_proba(X_scaled)
     
     sig_predicted_train = y_predict_train[y_train==0]
     sig_predicted_test = y_predict_test[y_test==0]
@@ -181,29 +138,27 @@ def startPlot(modelDir, binning=[50,0,1.], save=False, multiclass=True):
     sig_w = dataset['w'][y==0]
     bkg_predicted= y_predict[y!=0]
     bkg_w = dataset['w'][y!=0]
+   
+    print  y_predict.shape, y_predict[:5]
+    #outputScore = y_predict[:,0]
     
-    outputScore = y_predict[:,0]
-    
-    vars_2D = [
-            {'x':'met','y':'mt','xlabel':r'$E_{T}^{miss}$','ylabel':r'$m_{T}$', 'xbinning':[100,1000,80], 'ybinning':[90,700,80], 'xmev':True, 'ymev':True},
-            {'x':'met_proj_lep', 'y':'dphi_met_lep','xlabel':r'$E_{T,l}^{miss}$', 'ylabel':r'$\Delta\Phi(l, E_{T}^{miss})$', 'xbinning':[90,700,100], 'ybinning': [0,3.2,100], 'xmev':True, 'ymev':False},
-            {'x':'met','y':'jet_pt[0]','xlabel':r'$E_{T}^{miss}$', 'ylabel':r'$p_{T}^{jet0}$', 'xbinning':[100,1000,80], 'ybinning': [25,1000,80], 'xmev':True, 'ymev':True},
-            {'x':'amt2','y':'m_bl', 'xlabel':r'$am_{T2}$ [GeV]', 'ylabel':r'$m_{b,l}$', 'xbinning':[90,700,100], 'ybinning': [0,700,100], 'xmev':False, 'ymev':True},
-            {'x':'dr_bjet_lep','y':'m_bl', 'xlabel':r'$\Delta R(b,l)$', 'ylabel':r'$m_{b,l}$', 'xbinning':[0,3.2,100], 'ybinning': [0,1000,100], 'xmev':False, 'ymev':True},
-            {'x':'met', 'y':'ht','xlabel':r'$E_{T}^{miss}$', 'ylabel':r'$h_{T}$', 'xbinning':[100,1000,80], 'ybinning': [100,1000,80], 'xmev':True, 'ymev':True},
-            {'y':'mt', 'x':'ht','ylabel':r'$m_{T}$', 'xlabel':r'$h_{T}$', 'ybinning':[90,700,80], 'xbinning': [100,1000,80], 'ymev':True, 'xmev':True},
-            {'x':'met','y':'bjet_pt[0]','xlabel':r'$E_{T}^{miss}$','ylabel':r'$p_{T}^{bjet0}$', 'xbinning':[100,1000,80], 'ybinning':[25,1000,80], 'xmev':True, 'ymev':True},
-            {'x':'met_proj_lep','y':'mt','xlabel':r'$E_{T,l}^{miss}$','ylabel':r'$m_{T}$', 'xbinning':[100,1000,80], 'ybinning':[90,700,80], 'xmev':True, 'ymev':True}
-            ]
+#    vars_2D = [
+#            {'x':'met','y':'mt','xlabel':r'$E_{T}^{miss}$','ylabel':r'$m_{T}$', 'xbinning':[100,1000,80], 'ybinning':[90,700,80], 'xmev':True, 'ymev':True},
+#            {'x':'met_proj_lep', 'y':'dphi_met_lep','xlabel':r'$E_{T,l}^{miss}$', 'ylabel':r'$\Delta\Phi(l, E_{T}^{miss})$', 'xbinning':[90,700,100], 'ybinning': [0,3.2,100], 'xmev':True, 'ymev':False},
+#            {'x':'met','y':'jet_pt[0]','xlabel':r'$E_{T}^{miss}$', 'ylabel':r'$p_{T}^{jet0}$', 'xbinning':[100,1000,80], 'ybinning': [25,1000,80], 'xmev':True, 'ymev':True},
+#            {'x':'amt2','y':'m_bl', 'xlabel':r'$am_{T2}$ [GeV]', 'ylabel':r'$m_{b,l}$', 'xbinning':[90,700,100], 'ybinning': [0,700,100], 'xmev':False, 'ymev':True},
+#            {'x':'dr_bjet_lep','y':'m_bl', 'xlabel':r'$\Delta R(b,l)$', 'ylabel':r'$m_{b,l}$', 'xbinning':[0,3.2,100], 'ybinning': [0,1000,100], 'xmev':False, 'ymev':True},
+#            {'x':'met', 'y':'ht','xlabel':r'$E_{T}^{miss}$', 'ylabel':r'$h_{T}$', 'xbinning':[100,1000,80], 'ybinning': [100,1000,80], 'xmev':True, 'ymev':True},
+#            {'y':'mt', 'x':'ht','ylabel':r'$m_{T}$', 'xlabel':r'$h_{T}$', 'ybinning':[90,700,80], 'xbinning': [100,1000,80], 'ymev':True, 'xmev':True},
+#            {'x':'met','y':'bjet_pt[0]','xlabel':r'$E_{T}^{miss}$','ylabel':r'$p_{T}^{bjet0}$', 'xbinning':[100,1000,80], 'ybinning':[25,1000,80], 'xmev':True, 'ymev':True},
+#            {'x':'met_proj_lep','y':'mt','xlabel':r'$E_{T,l}^{miss}$','ylabel':r'$m_{T}$', 'xbinning':[100,1000,80], 'ybinning':[90,700,80], 'xmev':True, 'ymev':True}
+#            ]
     
     #Do various plots
     print 'Start plotting...'
     
     #TrainTestScore for cuts that are used for training
     plot_TrainTest_score.plot_TrainTest_score(sig_predicted_train[:,0], sig_predicted_test[:,0], sig_w_train, sig_w_test, bkg_predicted_train[:,0], bkg_predicted_test[:,0], bkg_w_train, bkg_w_test, binning, normed=1,save=save,fileName=filenames)
-    
-    #TrainTestScore for cuts that are manually specified
-    #plot_TrainTest_score.plot_TrainTest_score(sig_predicted_train[:,0][X_train[:,variables.index('met')][y_train==0]>=250e3], sig_predicted_test[:,0][X_test[:,variables.index('met')][y_test==0]>=250e3], sig_w_train[X_train[:,variables.index('met')][y_train==0]>=250e3], sig_w_test[X_test[:,variables.index('met')][y_test==0]>=250e3], bkg_predicted_train[:,0][X_train[:,variables.index('met')][y_train!=0]>=250e3], bkg_predicted_test[:,0][X_test[:,variables.index('met')][y_test!=0]>=250e3], bkg_w_train[X_train[:,variables.index('met')][y_train!=0]>=250e3], bkg_w_test[X_test[:,variables.index('met')][y_test!=0]>=250e3], binning, normed=1,save=save,fileName=filenames, addStr='met250')
     
     #CMs for cuts that are used for training
     plt.figure()
@@ -212,36 +167,18 @@ def startPlot(modelDir, binning=[50,0,1.], save=False, multiclass=True):
     plt.figure()
     plot_ConfusionMatrix.plot_confusion_matrix(y_train, y_predict_train, filename=filenames, save=save, isTrain=True)
     
-    #CMs for cuts that are manually specified
-    #print 'Plotting the CMs for met250'
-    
-    #plt.figure()
-    #plot_ConfusionMatrix.plot_confusion_matrix(y_test[X_test[:,variables.index('met')]>=250e3], y_predict_test[X_test[:,variables.index('met')]>=250e3], filename=filenames, save=save, isTrain=False, addStr='met250')
-    
-    #plt.figure()
-    #plot_ConfusionMatrix.plot_confusion_matrix(y_train[X_train[:,variables.index('met')]>=250e3], y_predict_train[X_train[:,variables.index('met')]>=250e3], filename=filenames, save=save, isTrain=True, addStr='met250')
-    
-    #Learning Curve
-    plt.figure()
-    plot_learning_curve.learning_curve_for_keras(pickleDir, save=save, filename=filenames)
+    if 'nn' in analysis.lower():
+      #Learning Curve
+      plt.figure()
+      plot_learning_curve.learning_curve_for_keras(pickleDir, save=save, filename=filenames)
     
     #Pie Charts
     #plt.figure()
     #plot_piechart.plot_pie_chart(y, y_predict, w, fileName=filenames, save=save)
     
-    # Output Score for cuts that are used in training for certain datapoints and additionally specified cuts
-    #plot_output_score.plot_output_score_datapoint(Signal1, model, preselection, variables, weights, lumi, binning, save=save, fileName=filenames)
-    
     #Output Score for cuts that are used in training
     plot_output_score.plot_output_score(sig_predicted[:,0], sig_w, bkg_predicted[:,0], bkg_w, binning, save=save, fileName=filenames, log=True)
     plot_output_score.plot_output_score(sig_predicted[:,0], sig_w, bkg_predicted[:,0], bkg_w, binning, save=save, fileName=filenames, log=False)
-    
-    #Output Score for cuts that are manually specified
-    #print 'Plotting the output score for met250'
-    #plot_output_score.plot_output_score(sig_predicted[:,0][X[:,variables.index('met')][y==0]>=250e3], sig_w[X[:,variables.index('met')][y==0]>=250e3], bkg_predicted[:,0][X[:,variables.index('met')][y!=0]>=250e3], bkg_w[X[:,variables.index('met')][y!=0]>=250e3], binning, save=save, fileName=filenames, addStr='_met250',log=True)
-    #plot_output_score.plot_output_score(sig_predicted[:,0][X[:,variables.index('met')][y==0]>=250e3], sig_w[X[:,variables.index('met')][y==0]>=250e3], bkg_predicted[:,0][X[:,variables.index('met')][y!=0]>=250e3], bkg_w[X[:,variables.index('met')][y!=0]>=250e3], binning, save=save, fileName=filenames, addStr='_met250',log=False)
-   
-    #plot_output_score2D.plot_output_score2D(variables, vars_2D, outputScore, X, y, save=save, fileName=filenames)
     
     plot_Correlation.plotCorrelation(X, y_predict, y, variables, fileName=filenames, save=save, multiclass=multiclass)
     #plot_Correlation.plotCorrelation(X, y_predict, y, variables, fileName=filenames, save=save, plotEPD=False)
